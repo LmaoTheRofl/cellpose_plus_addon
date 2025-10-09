fe106784945bc7ded65c576ca736c760266f88f3
LmaoTheRofl <Zharkov.nik04@mail.ru>
2025-10-09 18:41:47 +0300

    model/data/train update

--- Data_manager.py
+++ Data_manager.py
@@ -24,15 +24,15 @@ class SegmentationDataset(Dataset):
         
         img_path = os.path.join(self.root_dir, img_name)
         subdir = os.path.join(self.root_dir, base_name)
-        
-        image = Image.open(img_path) #.convert("RGB")
+        # print(img_path)
+        image = Image.open(img_path).convert("RGB")
         
         labels_dir = os.path.join(subdir, 'labels')
         mask_files = sorted(os.listdir(labels_dir), key=lambda x: int(x.split('.')[0]))
         masks = []
         for mf in mask_files:
             mask_path = os.path.join(labels_dir, mf)
-            mask = Image.open(mask_path) #.convert('L')
+            mask = Image.open(mask_path).convert('L')
             masks.append(mask)
         
         primary_dir = os.path.join(subdir, 'primary')
@@ -45,23 +45,30 @@ class SegmentationDataset(Dataset):
 
         mask_tensors = []
         boxes = []
+        W, H = image.size[0], image.size[1]
         for mask in masks:
-            mask_tensor = self.transform(mask)  # [1 x H x W], float
+            mask_tensor = self.transform(mask)  # [1 x H x W] одна маска
             mask_tensor = (mask_tensor > 0).float() 
             
             if mask_tensor.sum() == 0: 
                 continue
             y_indices, x_indices = torch.nonzero(mask_tensor[0] > 0, as_tuple=True)
-            x_min, x_max = x_indices.min(), x_indices.max()
-            y_min, y_max = y_indices.min(), y_indices.max()
-            boxes.append([x_min.item(), y_min.item(), x_max.item() + 1, y_max.item() + 1])  # [xmin, ymin, xmax, ymax]
+            if len(x_indices) == 0 or len(y_indices) == 0:
+                continue
+            x_min = max(0, x_indices.min().item())
+            x_max = min(W, x_indices.max().item() + 1)
+            y_min = max(0, y_indices.min().item())
+            y_max = min(H, y_indices.max().item() + 1)
+            
+
+            boxes.append([x_min, y_min, x_max, y_max])  
             
             mask_tensors.append(mask_tensor)
 
         if len(mask_tensors) == 0:
             target = {'boxes': torch.empty((0, 4), dtype=torch.float32),
                       'labels': torch.empty((0,), dtype=torch.int64),
-                      'masks': torch.empty((0, image.size[1], image.size[0]), dtype=torch.uint8)}
+                      'masks': torch.empty((0, H, W), dtype=torch.uint8)}
         else:
             target = {
                 'boxes': torch.tensor(boxes, dtype=torch.float32),
