5c95d132607b8aaaa80675939f15a1abae57773b
LmaoTheRofl <Zharkov.nik04@mail.ru>
2025-10-24 12:38:02 +0300

    Nuclei update

--- Data_manager.py
+++ Data_manager.py
@@ -1,89 +1,83 @@
-import pandas as pd
-import numpy as np
 import os
+import numpy as np
 from torch.utils.data import Dataset
-from PIL import Image
 import torch
-import torchvision
-from torchvision.transforms import ToTensor, Compose, ConvertImageDtype
-import torch.nn.functional as F
+from PIL import Image
+import tifffile
+from cellpose_plus import transforms
+import cv2
 
+class NucleiDataset(Dataset):
+    def __init__(self, root_dir, resize_to=None, verbose=False):
 
-class MaskRCNNDataset(Dataset):
-    def __init__(self, root_dir, transform=None):
         self.root_dir = root_dir
-        self.transform = transform or Compose([ToTensor(), ConvertImageDtype(torch.float)])
+        self.resize_to = resize_to
+        self.verbose = verbose
+
         self.images = sorted([f for f in os.listdir(root_dir) if f.endswith('.tif')])
-    
+
     def __len__(self):
         return len(self.images)
-    
+
+    def load_image(self, path):
+        img = tifffile.imread(path).astype(np.float32)
+        if img.ndim == 3:
+            if img.shape[-1] <= 4:
+                img = np.mean(img, axis=-1)
+            elif img.shape[0] <= 4:
+                img = np.mean(img, axis=0)
+            else:
+                img = np.max(img, axis=0)
+
+        img = (img - img.min()) / (img.max() - img.min() + 1e-8)
+        img = np.power(img, 0.8).astype(np.float32)
+        return np.stack([img, np.zeros_like(img)], axis=0)
+
+    def load_masks(self, base_name, H, W):
+        labels_dir = os.path.join(self.root_dir, base_name, "labels")
+        mask_files = sorted(os.listdir(labels_dir), key=lambda x: int(x.split('.')[0]))
+        combined = np.zeros((H, W), dtype=np.int32)
+        for i, mf in enumerate(mask_files):
+            mask = np.array(Image.open(os.path.join(labels_dir, mf)).convert("L"))
+            if mask.shape != (H, W):
+                mask = np.array(Image.fromarray(mask).resize((W, H), resample=Image.NEAREST))
+            combined[mask > 0] = i + 1
+        return combined
+
     def __getitem__(self, idx):
         img_name = self.images[idx]
         base_name = os.path.splitext(img_name)[0]
-        
         img_path = os.path.join(self.root_dir, img_name)
-        subdir = os.path.join(self.root_dir, base_name)
-        # print(img_path)
-        image = Image.open(img_path).convert("RGB")
+
+        image = self.load_image(img_path)
+        H, W = image.shape[-2:]
+        combined_masks = self.load_masks(base_name, H, W)
+
+        if self.resize_to:
+            image = transforms.resize_image(image, Ly=self.resize_to[0], Lx=self.resize_to[1], interpolation=1)
+            combined_masks = transforms.resize_image(combined_masks, Ly=self.resize_to[0], Lx=self.resize_to[1],
+                                                    interpolation=0, no_channels=True)
+        def pad_32(arr, div=32):
+            H, W = arr.shape[-2:]
+            pad_h = (div - H % div) % div
+            pad_w = (div - W % div) % div
+            if pad_h > 0 or pad_w > 0:
+                if arr.ndim == 3:
+                    arr = np.pad(arr, ((0, 0), (0, pad_h), (0, pad_w)), mode='reflect')
+                elif arr.ndim == 2:
+                    arr = np.pad(arr, ((0, pad_h), (0, pad_w)), mode='reflect')
+            return arr
+        image = pad_32(image)
+        combined_masks = pad_32(combined_masks)
         
-        labels_dir = os.path.join(subdir, 'labels')
-        mask_files = sorted(os.listdir(labels_dir), key=lambda x: int(x.split('.')[0]))
-        masks = []
-        for mf in mask_files:
-            mask_path = os.path.join(labels_dir, mf)
-            mask = Image.open(mask_path).convert('L')
-            masks.append(mask)
+        cellprob = (combined_masks > 0).astype(np.float32) 
         
-        primary_dir = os.path.join(subdir, 'primary')
-        center_csv = os.path.join(primary_dir, base_name + '_Center.csv')
-        size_roundness_csv = os.path.join(primary_dir, base_name + '_size_roundness.csv')
+        gy, gx = np.gradient(cellprob) # cellprob - бинарная маска, получаем градиент для вычисления вертикального потока и горизонтального, которые требуются в cellpose моделях
+        dy, dx = -gy, -gx  
+        lbl = np.stack([cellprob, dy, dx], axis=0).astype(np.float32)
+
+        # if self.verbose:
+        #     mask_px = np.count_nonzero(cellprob)
+        #     print(f"[{img_name}] mask_pixels={mask_px} (NO FLOW MODE)")
         
-        info = pd.concat([pd.read_csv(center_csv, names=['x', 'y']), pd.read_csv(size_roundness_csv, names=['size', 'roundness'])], axis = 1)
-        info['classes'] = info['size'].apply(lambda x: 1 if x <= 40 else 2)
-        # info = info.to_numpy()
-
-        mask_tensors = []
-        boxes = []
-        W, H = image.size[0], image.size[1]
-        for mask in masks:
-            mask_tensor = self.transform(mask)  # [1 x H x W] одна маска
-            mask_tensor = (mask_tensor > 0).float() 
-            
-            if mask_tensor.sum() == 0: 
-                continue
-            y_indices, x_indices = torch.nonzero(mask_tensor[0] > 0, as_tuple=True)
-            if len(x_indices) == 0 or len(y_indices) == 0:
-                continue
-            x_min = max(0, x_indices.min().item())
-            x_max = min(W, x_indices.max().item() + 1)
-            y_min = max(0, y_indices.min().item())
-            y_max = min(H, y_indices.max().item() + 1)
-            
-
-            boxes.append([x_min, y_min, x_max, y_max])  
-            
-            mask_tensors.append(mask_tensor)
-
-        if len(mask_tensors) == 0:
-            target = {'boxes': torch.empty((0, 4), dtype=torch.float32),
-                      'labels': torch.empty((0,), dtype=torch.int64),
-                      'masks': torch.empty((0, H, W), dtype=torch.uint8)}
-        else:
-            target = {
-                'boxes': torch.tensor(boxes, dtype=torch.float32),
-                'labels': torch.tensor(info['classes'].values[:len(boxes)], dtype=torch.int64), 
-                'masks': torch.stack(mask_tensors)  # [N x 1 x H x W]
-            }
-            target['masks'] = target['masks'].squeeze(1)  # [N x H x W]
-        
-        image = self.transform(image)
-        
-
-        return image, target
-
-def mask_collate_fn(batch):
-    images = [item[0] for item in batch]
-    targets = [item[1] for item in batch]
-    return images, targets
-
+        return torch.from_numpy(image).float(), torch.from_numpy(lbl).float()
